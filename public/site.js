62567fffb90540bcfd4931d3a7a5710fd3fdb9cb
Pedro Coelho <pedro.luis.coelho@gmail.com>
2013-08-22 12:01:02 +0100

    Small tweaks in the static assets

---
+++ public/site.js
@@ -0,0 +1,23 @@
+var getLongenedURL = function (url, callback) {
+  var req = new XMLHttpRequest();
+  req.onload = function () {
+    callback(window.location + this.responseText);
+  }
+
+  req.open('GET', '/create?url=' + url)
+  req.send()
+}
+
+document.getElementById('new_url').onsubmit = function () {
+  var result_url = document.getElementById('result_url');
+  var result     = document.getElementById('result');
+
+  result_url.innerHTML = 'loading...';
+  result.style.display = 'block';
+
+  getLongenedURL(document.getElementById('new_url_url').value, function (url) {
+    result_url.innerHTML = url;
+  });
+
+  return false;
+}
