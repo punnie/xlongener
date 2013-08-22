29ec6c577e47132f3a4ee91055aac0a9653177a6
Pedro Coelho <pedro.luis.coelho@gmail.com>
2013-08-22 12:44:43 +0100

    Tweaked the entry page just a bit

--- public/site.js
+++ public/site.js
@@ -12,11 +12,12 @@ document.getElementById('new_url').onsubmit = function () {
   var result_url = document.getElementById('result_url');
   var result     = document.getElementById('result');
 
-  result_url.innerHTML = 'loading...';
+  result_url.value = 'loading...';
   result.style.display = 'block';
 
   getLongenedURL(document.getElementById('new_url_url').value, function (url) {
-    result_url.innerHTML = url;
+    result_url.value = url;
+    result_url.select();
   });
 
   return false;
